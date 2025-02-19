7407c87fd814127c7e6bd66f9528c3ebab6984c7
cpminipc <cpierres@hotmail.com>
2025-02-19 16:08:59 +0100

    Tests simples sur login.component

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -39,4 +39,44 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('doit avoir les valeurs par défaut initialisées', () => {
+    expect(component.hide).toBe(true); // "hide" est par défaut vrai
+    expect(component.onError).toBe(false); // Aucune erreur au début
+  });
+
+  it('doit créer un formulaire avec les contrôles email et password', () => {
+    expect(component.form.contains('email')).toBeTruthy();
+    expect(component.form.contains('password')).toBeTruthy();
+  });
+
+  it('doit invalider le formulaire si les champs sont vides', () => {
+    component.form.setValue({
+      email: '',
+      password: ''
+    });
+    expect(component.form.invalid).toBeTruthy();
+  });
+
+  it('email doit être invalide si le format est incorrect', () => {
+    const emailControl = component.form.get('email');
+    emailControl?.setValue('toto@@toto.com');
+    expect(emailControl?.invalid).toBeTruthy();
+  });
+
+  it('doit afficher un message d’erreur si onError est vrai', () => {
+    component.onError = true;
+    fixture.detectChanges(); // Met à jour le DOM
+    const errorMessage = fixture.nativeElement.querySelector('.error');
+    expect(errorMessage).toBeTruthy();
+    expect(errorMessage.textContent).toContain('An error occurred');
+  });
+
+  it('devrait appeler la méthode submit() lors de la soumission du formulaire', () => {
+    jest.spyOn(component, 'submit');
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit')); // Simule un événement de soumission
+    expect(component.submit).toHaveBeenCalledTimes(1);
+  });
+
 });

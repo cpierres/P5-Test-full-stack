f1d3bfe00923b72c7cd3468836db56c3f05527ef
cpminipc <cpierres@hotmail.com>
2025-02-28 11:26:37 +0100

    config cypress : var d'environnement pour utiliser les tests e2e soit en vrai backend, soit en backend mocked

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
   video: false,
+  env: {
+    useRealBackend: process.env['CYPRESS_USE_REAL_BACKEND'] === 'true',
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -12,5 +15,8 @@ export default defineConfig({
       return require('./cypress/plugins/index.ts').default(on, config)
     },
     baseUrl: 'http://localhost:4200',
+    // baseUrl: process.env['CYPRESS_USE_REAL_BACKEND'] === 'true'
+    //   ? 'https://real-backend-url.com' // Backend réel
+    //   : 'http://localhost:4200', // Backend mocké/local
   },
 })

83b88eb83a075a19d119b17e4205d0ec0d55d335
cpminipc <cpierres@hotmail.com>
2025-03-06 09:56:04 +0100

    UserDetailsServiceImplIntegrationTest

---
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsServiceImplIntegrationTest.java
@@ -0,0 +1,62 @@
+package com.openclassrooms.starterjwt.security.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+
+class UserDetailsServiceImplIntegrationTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testLoadUserByUsername_Success() {
+        String email = "user@test.com";
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setEmail(email);
+        mockUser.setFirstName("Prénom");
+        mockUser.setLastName("Nom");
+        mockUser.setPassword("test!1234");
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(mockUser));
+
+        var userDetails = userDetailsService.loadUserByUsername(email);
+
+        assertEquals(email, userDetails.getUsername());
+        assertEquals(mockUser.getPassword(), userDetails.getPassword());
+    }
+
+    @Test
+    void testLoadUserByUsername_UserNotFound() {
+        String email = "inconnu@test.com";
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(UsernameNotFoundException.class, () -> {
+            userDetailsService.loadUserByUsername(email);
+        });
+
+        assertEquals("User Not Found with email: " + email, exception.getMessage());
+    }
+}
